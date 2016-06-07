ce858b289bb67bb88752d8b5bb4c0a46b527982a
Jim O'Donnell <jim@zooniverse.org>
2016-06-07 18:04:04 +0100

    Test log requests Fake Google Analytics dataLayer (otherwise the client dies.) Add support for XMLHttpRequest in tests.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,5 @@
+global.XMLHttpRequest = require('xhr2');
+global.dataLayer = [];
 var test = require('blue-tape');
 var GeordiClient = require('../index');
 
@@ -6,4 +8,16 @@ test('Instantiate a client with default settings', function(t) {
   t.equal(geordi.env, 'staging');
   t.equal(geordi.projectToken, 'unspecified');
   t.end()
+});
+test('Log without a valid project token', function(t) {
+  var geordi = new GeordiClient({projectToken:''});
+  geordi.logEvent('test event')
+    .then(function(response){
+      t.fail('invalid project token should not be logged');
+      t.end()
+    })
+    .catch(function(error){
+      t.pass(error);
+      t.end()
+    });
 });
