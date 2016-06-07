61323db8ef27e1407abbbf3efbc8fb772896d59e
Jim O'Donnell <jim@zooniverse.org>
2016-06-07 17:48:50 +0100

    update tests

--- test/index.js
+++ test/index.js
@@ -2,8 +2,8 @@ var test = require('blue-tape');
 var GeordiClient = require('../index');
 
 test('Instantiate a client with default settings', function(t) {
-  var geordi = new GeordiClient({projectToken: ''});
-  t.equal(geordi.GEORDI_SERVER_URL, 'https://geordi.staging.zooniverse.org/api/events/');
-  t.equal(geordi._projectToken, 'unspecified');
+  var geordi = new GeordiClient();
+  t.equal(geordi.env, 'staging');
+  t.equal(geordi.projectToken, 'unspecified');
   t.end()
 });
